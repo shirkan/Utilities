7a5a37f53ddb342245439b62858c36ca7daa7c12
Liran Cohen <liran.totem@gmail.com>
2016-05-29 17:19:42 +0300

    universe switches v1.1
    enable single or multiple ids by using -ids
    set ads to all and all_but_admob

--- universe_switches.py
+++ universe_switches.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python -u
 import argparse, mechanize, cookielib, sys, os
 from lxml import html
 
@@ -8,8 +8,12 @@ LOGIN_URL = "http://universe.appninjaz.com/login"
 LOGIN_USER_ELEMENT = "identity"
 LOGIN_PASSWORD_ELEMENT = "password"
 
-# for each template, which fields numbers to change
-TEMPLATES = {
+# enable webview with affiliations - for each template, which fields numbers to change
+WEBVIEW_TRIGGERS = {
+    'ios' : ["custom_ad_1_enabled", "custom_ad_2_enabled", "custom_ad_3_enabled", "enable_custom_ad_lobby", "is_free_coins_btn_live", "is_live"],
+    'android' : ["custom_ad_1_enabled", "custom_ad_2_enabled", "custom_ad_3_enabled", "enable_custom_ad_lobby", "is_live"]
+}
+WEBVIEW_FIELDS_IN_TEMPLATES = {
     'Slot v2' : [17, 23, 29, 42, 56, 57],
     'Slots V2 Tola' : [16, 22, 28, 41, 55, 56],
     'Slots V2 Android' : [16, 22, 28, 39, 49],
@@ -17,12 +21,30 @@ TEMPLATES = {
     'Slots V3 Android' : [16, 22, 28, 39, 49],
     'Slots V3 Scam' : [17, 23, 29, 42, 56, 57]
 }
-APPS_PAGE = "http://universe.appninjaz.com/applications"
-TRIGGERS = {
-    'ios' : ["custom_ad_1_enabled", "custom_ad_2_enabled", "custom_ad_3_enabled", "enable_custom_ad_lobby", "is_free_coins_btn_live", "is_live"],
-    'android' : ["custom_ad_1_enabled", "custom_ad_2_enabled", "custom_ad_3_enabled", "enable_custom_ad_lobby", "is_live"]
+
+# enable all ads
+ADS_IDS = {
+    'admob' : '54274c6f39e97a65628b4569',
+    'applovin' : '54274c6f39e97a65628b456a',
+    'chartboost' : '54274c6f39e97a65628b456b',
+    'playhaven' : '54274c6f39e97a65628b456c'
 }
 
+ADS_TEMPLATES = {
+    'all' : [ADS_IDS['admob'], ADS_IDS['applovin'], ADS_IDS['chartboost'], ADS_IDS['playhaven']],
+    'all_but_admob' : [ADS_IDS['applovin'], ADS_IDS['chartboost'], ADS_IDS['playhaven']]
+}
+
+ADS_TRIGGERS = {
+    3 : 'all_but_admob',
+    4 : 'all',
+    5 : 'all',
+    6 : 'all',
+    7 : 'all',
+    8 : 'all'
+}
+
+APPS_PAGE = "http://universe.appninjaz.com/applications"
 APP_PAGE_PREFIX = "http://universe.appninjaz.com/applications/"
 APP_PAGE_SUFFIX = "/edit"
 ONLINE = "Integrated"
@@ -30,10 +52,17 @@ ONLINE = "Integrated"
 # Parse inputs
 parser = argparse.ArgumentParser(description='Create parse app and retrieve info')
 # start from app
-parser.add_argument('-startFrom', default="", required=False, help='Game name')
+parser.add_argument('-startFrom', default="", required=False, help='Start editing from game id and on')
+parser.add_argument('-ids', required=False, dest="ids", help='Edit specific id(s), seperated by commas')
 
 args = parser.parse_args()
 startFrom = args.startFrom
+ids = args.ids
+
+if startFrom != "" and ids != "":
+    print "Cannot run -ids and -startFrom together..."
+    sys.exit()
+ids = ids.split(",")
 
 def getCredentials(inputFile):
     with open(inputFile) as inFile:
@@ -41,7 +70,7 @@ def getCredentials(inputFile):
             user, password = line.split()
     return (user, password)
 
-print "Universe set all switches - by Liran Cohen V1.0"
+print "Universe set all switches - by Liran Cohen V1.1"
 
 # Get credentials
 user, password = getCredentials(PASSWORDS_FILE)
@@ -90,9 +119,12 @@ for elem in elems:
         else:
             continue
 
+    if len(ids) > 0 and not id in ids:
+        continue
+
     #if we got here, we can check
     # check if type can be handled
-    if not type in TEMPLATES:
+    if not type in WEBVIEW_FIELDS_IN_TEMPLATES:
         continue
 
     # edit app
@@ -100,8 +132,19 @@ for elem in elems:
     res = browser.open(editLink, timeout=15.0)
     browser.select_form(nr = 0)
 
+    # i = 0
+    # for control in browser.form.controls:
+    #     print str(i) + ":" + str(control)
+    #     print "type=%s, name=%s value=%s" % (control.type, control.name, browser[control.name])
+    #     i+=1
+    # break
+
+    if (status == ONLINE):
+        for ad_num in ADS_TRIGGERS:
+            browser.form.controls[ad_num].value = ADS_TEMPLATES[ADS_TRIGGERS[ad_num]]
+
     value = ('1' if status == ONLINE else '0')
-    for num in TEMPLATES[type]:
+    for num in WEBVIEW_FIELDS_IN_TEMPLATES[type]:
         # print "before " + str(browser.form.controls[num]) + " - " + str(browser.form.controls[num].value)
         browser.form.controls[num].value = [value]
         # print "after " + str(browser.form.controls[num]) + " - " + str(browser.form.controls[num].value)
